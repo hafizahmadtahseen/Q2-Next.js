55cdcb1c71d6df78d63f127d607b7e8e82ac7eee
hafizahmadtahseen <h.a.tahseen@gmail.com>
2024-03-22 01:44:28 +0500

    Commit Message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
-export default function Home() {
-  return (
-        <div>Hello World</div>
-  );
+function HomePage() {
+  return <h1>Hello, I am Hafiz Ahmad Tahseen and this is my first Next.js project.</h1>
 }
+export default HomePage;
